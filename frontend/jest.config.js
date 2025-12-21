395bd0682e1b0a9421fa944d71bc1fb2ee50a123
Abhi6310 <abch4744@colorado.edu>
2025-12-20 20:27:46 -0700

    created actions workflow and tests for build images, backend, and frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx}',
+    'components/**/*.{js,jsx}',
+    'lib/**/*.{js,jsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+};
